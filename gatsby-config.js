99a2dc0eb944781783645c1b8be9586b724a1246
gejiaheng <gejiaheng@gmail.com>
2020-06-01 21:24:01 -0700

    add theme-ui dependencies and format code

--- gatsby-config.js
+++ gatsby-config.js
@@ -116,6 +116,7 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    "gatsby-plugin-theme-ui",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
