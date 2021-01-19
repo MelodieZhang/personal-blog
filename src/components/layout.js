1f613bee2f0b1c83cb5819ba84756f5e93516f1d
Jiaheng Ge <gejiaheng@gmail.com>
2021-01-19 11:34:41 -0800

    Replace Theme UI with Tailwindcss for index page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,35 +1,15 @@
 import React from "react"
-
-import { rhythm } from "../utils/typography"
 import TopNav from "./top-nav"
 
 const Layout = ({ location, children }) => {
   return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-      }}
-    >
+    <div className="max-w-prose mx-auto p-6">
       <header>
         <TopNav location={location}></TopNav>
       </header>
       <main>{children}</main>
-      <footer
-        style={{
-          textAlign: `center`,
-          marginTop: `5.25rem`,
-        }}
-      >
-        <div
-          style={{
-            color: `rgba(0, 0, 0, 0.4)`,
-            fontSize: `0.875rem`,
-            marginTop: `5rem`,
-          }}
-        >
+      <footer className="text-center my-8">
+        <div className="text-sm">
           Melodie &copy; {new Date().getFullYear()} · All rights reserved
         </div>
       </footer>
