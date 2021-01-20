60e69fc429289d427b4bf0a3fb52488bfdea5e6e
Jiaheng Ge <gejiaheng@gmail.com>
2021-01-19 16:18:11 -0800

    Replace Theme UI with Tailwindcss for about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,4 @@
-/** @jsx jsx */
-import { jsx } from "theme-ui"
+import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
@@ -32,12 +31,6 @@ const AboutPage = ({ location }) => {
     }
   `)
 
-  const aboutHeading = {
-    variant: `text.heading`,
-    fontSize: `3`,
-    marginBottom: `4`,
-  }
-
   const { author, siteTitle } = data.site.siteMetadata
 
   return (
@@ -46,21 +39,11 @@ const AboutPage = ({ location }) => {
         title="About"
         description="Personal information about Menghan Zhang"
       />
-      <div
-        sx={{
-          display: `flex`,
-          flexWrap: `wrap`,
-          justifyContent: `space-between`,
-        }}
-      >
+      <div className="flex justify-between">
         <div>
-          <div
-            sx={{
-              marginBottom: `6`,
-            }}
-          >
-            <h3 sx={{ ...aboutHeading }}>About</h3>
-            <p sx={{ variant: `text.body` }}>
+          <div className="mb-10">
+            <h2 className="mb-4">About</h2>
+            <p className="leading-loose">
               张梦晗, Melodie
               <br />
               Google 用户体验设计师
@@ -70,56 +53,59 @@ const AboutPage = ({ location }) => {
               喜欢写字的普通人
             </p>
           </div>
-          <div
-            sx={{
-              marginBottom: `6`,
-            }}
-          >
-            <h3 sx={{ ...aboutHeading }}>Elsewhere</h3>
-            <p sx={{ variant: `text.body` }}>
-              <a
-                href="https://us10.campaign-archive.com/home/?u=4fed45ee5092478a3ecdc063a&id=391be5d253"
-                rel="noreferrer"
-                target="_blank"
-              >
-                Mélodie Newsletter
-              </a>
-              <div>隔周更新，包含七个链接</div>
-              <a
-                href="https://www.etsy.com/shop/DreamZakka"
-                rel="noreferrer"
-                target="_blank"
-              >
-                Dream Zakka
-              </a>
-              <div>贩卖手绘和设计的 Etsy 小店</div>
-              <a
-                href="https://www.instagram.com/menghan.xyz/"
-                rel="noreferrer"
-                target="_blank"
-              >
-                menghan.xyz
-              </a>
-              <div>不定期更新的插画</div>
-              <a
-                href="https://mydesignnotebook.tumblr.com/"
-                rel="noreferrer"
-                target="_blank"
-              >
-                Design Notebook
-              </a>
-              <div>产品设计文章收集册</div>
+          <div className="mb-10">
+            <h2 className="mb-4">Elsewhere</h2>
+            <p className="leading-loose">
+              <div className="leading-relaxed">
+                <a
+                  href="https://us10.campaign-archive.com/home/?u=4fed45ee5092478a3ecdc063a&id=391be5d253"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  Mélodie Newsletter
+                </a>
+                <div>隔周更新，包含七个链接</div>
+              </div>
+
+              <div className="leading-relaxed">
+                <a
+                  href="https://www.etsy.com/shop/DreamZakka"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  Dream Zakka
+                </a>
+                <div>贩卖手绘和设计的 Etsy 小店</div>
+              </div>
+
+              <div className="leading-relaxed">
+                <a
+                  href="https://www.instagram.com/menghan.xyz/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  menghan.xyz
+                </a>
+                <div>不定期更新的插画</div>
+              </div>
+
+              <div className="leading-relaxed">
+                <a
+                  href="https://mydesignnotebook.tumblr.com/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  Design Notebook
+                </a>
+                <div>产品设计文章收集册</div>
+              </div>
             </p>
           </div>
-          <div
-            sx={{
-              marginBottom: `6`,
-            }}
-          >
-            <h3 sx={{ ...aboutHeading }}>Don't be strangers</h3>
-            <p sx={{ variant: `text.body` }}>
+          <div>
+            <h2 className="mb-4">Don't be strangers</h2>
+            <p className="leading-loose">
               <div>
-                <span sx={{ fontWeight: `bold` }}>Telegram&nbsp;&nbsp;</span>
+                <span className="font-bold">Telegram&nbsp;&nbsp;</span>
                 <a
                   href="https://telegram.me/menghanzhang"
                   title="telegram"
@@ -130,7 +116,7 @@ const AboutPage = ({ location }) => {
                 </a>
               </div>
               <div>
-                <span sx={{ fontWeight: `bold` }}>Email</span>
+                <span className="font-bold">Email</span>
                 &nbsp;&nbsp;zhangmenghan728@gmail.com
               </div>
               <br />
