8aabd1bcbccaca2f697a40004c2fe1f60d8e672b
Jiaheng Ge <gejiaheng@gmail.com>
2021-01-19 16:54:56 -0800

    Replace Mailchimp with Substack

---
+++ src/components/subscribe/newsletter-sub.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const NewsletterSub = () => {
+  return (
+    <iframe
+      src="https://menghan.substack.com/embed"
+      width="480"
+      height="320"
+      //   style="border:1px solid #EEE; background:white;"
+      //   frameborder="0"
+      scrolling="no"
+    />
+  )
+}
+
+export default NewsletterSub
