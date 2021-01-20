687dfb6caafbf96495fa0b71dd8d0572bebd1572
Jiaheng Ge <gejiaheng@gmail.com>
2021-01-19 16:21:07 -0800

    Replace Theme UI with Tailwindcss for Newsletter

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,5 +1,4 @@
-/** @jsx jsx */
-import { jsx } from "theme-ui"
+import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,22 +9,16 @@ const NewsletterPage = ({ location }) => {
   return (
     <Layout location={location}>
       <SEO title="Newsletter" description="Menghan's newsletter" />
-      <h3
-        sx={{
-          variant: `text.heading`,
-          fontSize: `3`,
-          marginBottom: `4`,
-        }}
-      >
+      <h2 className="mb-8">
         <Emoji symbol="📮" label="postbox" /> Mélodie
-      </h3>
-      <p sx={{ variant: `text.body` }}>
+      </h2>
+      <p>
         Mélodie
         是每两周更新一次的电子简报，分享我认为值得推荐的七个链接，主题不限，形式不定，可能是文章/书/视频/音乐/产品等任何有趣的事物。欢迎订阅
         :)
       </p>
       <SimpleSubscribe />
-      <p sx={{ variant: `text.body` }}>
+      <p>
         <a
           href="https://us10.campaign-archive.com/home/?u=4fed45ee5092478a3ecdc063a&id=391be5d253"
           rel="noreferrer"
