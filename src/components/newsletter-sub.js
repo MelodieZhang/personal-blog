076418fd13d7919c97ed96174e0986336dddd1c3
Jiaheng Ge <gejiaheng@gmail.com>
2021-01-19 22:29:32 -0800

    Style work

--- src/components/newsletter-sub.js
+++ src/components/newsletter-sub.js
@@ -2,15 +2,17 @@ import React from "react"
 
 const NewsletterSub = () => {
   return (
-    <iframe
-      title="Melodie Newsletter subscription"
-      src="https://menghan.substack.com/embed"
-      width="480"
-      height="320"
-      //   style="border:1px solid #EEE; background:white;"
-      //   frameborder="0"
-      scrolling="no"
-    />
+    <div className="flex justify-center">
+      <iframe
+        title="Melodie Newsletter subscription"
+        src="https://menghan.substack.com/embed"
+        width="480"
+        height="320"
+        //   style="border:1px solid #EEE; background:white;"
+        //   frameborder="0"
+        scrolling="no"
+      />
+    </div>
   )
 }
 
