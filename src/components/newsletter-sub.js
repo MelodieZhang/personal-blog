3031ca84fcb9e423ba09d4e92eb6be20204c6bab
Jiaheng Ge <gejiaheng@gmail.com>
2021-01-19 19:43:03 -0800

    Add title for iframe

--- src/components/newsletter-sub.js
+++ src/components/newsletter-sub.js
@@ -3,6 +3,7 @@ import React from "react"
 const NewsletterSub = () => {
   return (
     <iframe
+      title="Melodie Newsletter subscription"
       src="https://menghan.substack.com/embed"
       width="480"
       height="320"
